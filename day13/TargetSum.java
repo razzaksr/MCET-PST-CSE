2dfe48eb5acc280ceed473c3454b094c536c8a89
razzaksr <razzaksr@gmail.com>
2026-02-12 16:34:24 +0530

    day10 update

--- day13/TargetSum.java
+++ day13/TargetSum.java
@@ -6,17 +6,16 @@ public class TargetSum {
     public static int findTargetSum(int[] nums, int target){
         int sum = 0;
         sum = Arrays.stream(nums).sum();
-        if(Math.abs(sum)>sum) return 0;
+        if(Math.abs(target)>sum) return 0;
         if((target+sum)%2!=0) return 0;
         int subsetSum = (target+sum)/2;
         return countSubsetsFreq(nums, subsetSum);
     }
     public static int countSubsetsFreq(int[] nums, int target){
         int[] dp = new int[target+1];dp[0]=1;
-        for(int each:nums){
+        for(int each:nums)
             for(int index=target;index>=each;index--)
                 dp[index] += dp[index-each];
-        }
         return dp[target];
     }
     public static void main(String[] args) {
